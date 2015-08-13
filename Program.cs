494b802f2a086caf9053df86dc47f3595a22d7dc
DD~! <devddstuff@gmail.com>
2015-08-12 21:48:17 -0400

    Fixes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 
 namespace Space
 {
@@ -8,20 +7,40 @@ namespace Space
         /* Settings */
         static char[] chars = { '*', '°' };
         static int density = 3;
-
+        /* Not settings */
         static Random ran = new Random(DateTime.Now.Millisecond);
-        static void Main(string[] args)
+        static readonly string ProjectVersion =
+            string.Format("{0}",
+            System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+
+        static int Main(string[] args)
         {
             if (args.Length > 0)
-                return;
+            {
+                ShowHelp();
+                return 0;
+            }
 
             for (int i = 0; i < args.Length; i++)
             {
                 switch (args[i])
                 {
-                    case "":
-
+                    case "-d":
+                        if (!int.TryParse(args[i + 1], out density))
+                        {
+                            Console.WriteLine("Invalid density number, aborting.");
+                            return 2;
+                        }
                         break;
+
+                    case "/?":
+                    case "--help":
+                        ShowHelp();
+                        return 0;
+
+                    case "--version":
+                        ShowVersion();
+                        return 0;
                 }
             }
 
@@ -50,6 +69,30 @@ namespace Space
             }
 
             Console.ReadKey(true);
+
+            return 0;
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine(" Usage:");
+            Console.WriteLine("  Space [options]");
+            Console.WriteLine();
+            Console.WriteLine("  -d     Density, 0-100. Default is 3.");
+            Console.WriteLine();
+            Console.WriteLine("  --help, /?      Shows this screen");
+            Console.WriteLine("  --version       Shows version");
+        }
+
+        static void ShowVersion()
+        {
+            Console.WriteLine("Space - " + ProjectVersion);
+            Console.WriteLine("Copyright (c) 2015 DD~!/guitarxhero");
+            Console.WriteLine("License: MIT License <http://opensource.org/licenses/MIT>");
+            Console.WriteLine("Project page: <https://github.com/guitarxhero/Space>");
+            Console.WriteLine();
+            Console.WriteLine(" -- Credits --");
+            Console.WriteLine("DD~! (guitarxhero) - Original author");
         }
     }
 } 
---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Space - Generate space!
+
+Just a quick recreation of my friend's (@pt300) space app that he done in C (ELF).
