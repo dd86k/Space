66d6c0e73b04d176a3af623eb47a5b5e721b3704
DD~! <devddstuff@gmail.com>
2015-08-12 21:39:24 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+
+namespace Space
+{
+    class Program
+    {
+        /* Settings */
+        static char[] chars = { '*', '°' };
+        static int density = 3;
+
+        static Random ran = new Random(DateTime.Now.Millisecond);
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+                return;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "":
+
+                        break;
+                }
+            }
+
+            int charsmax = chars.Length;
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            int colorsmax = colors.Length;
+
+            Console.Clear();
+
+            int Width = Console.WindowWidth;
+            int Height = Console.WindowHeight;
+            int rannum;
+            for (int w = 0; w < Width; w++)
+            {
+                for (int h = 0; h < Height; h++)
+                {
+                    rannum = ran.Next(0, 100);
+                    if (rannum < density)
+                    {
+                        Console.ForegroundColor = colors[ran.Next(0, colorsmax)];
+                        Console.Write(chars[ran.Next(0, charsmax)]);
+                    }
+                    else
+                        Console.Write(" ");
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+} 
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Space")]
+[assembly: AssemblyDescription("Generate space!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("guitarxhero")]
+[assembly: AssemblyProduct("Space")]
+[assembly: AssemblyCopyright("Copyright © guitarxhero 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("d8bd54bc-6763-4200-a0f1-1e1df16da29b")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
